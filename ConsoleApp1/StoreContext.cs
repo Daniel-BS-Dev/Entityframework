93d847b6d09d832ef711cecb2bbe33e9c17735cc
Daniel Silva <daniel.silva@onlineapp.com.br>
2022-05-09 08:23:26 -0300

    conection database

---
+++ ConsoleApp1/StoreContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConsoleApp1
+{
+    class StoreContext : DbContext
+    {
+
+        // sobreescrevendo o metodo. metodo de configuração
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // dizendo a minha maquina que quero usar o banco de dados. se caminho server... vem da minha classe dao
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=LojaDB;Trusted_Connection=true;");
+        }
+
+    }
+}
